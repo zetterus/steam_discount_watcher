fca83f5f5070d0040e0638d4d79578583ac07dc0
zetterus <zetter.vitrolic@gmail.com>
2024-10-06 13:42:20 +0300

    ваще нихера непонятно с этой авторизацией, но теперь она работает

--- authentication.py
+++ authentication.py
@@ -1,29 +1,58 @@
-import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
+"""
+Script description: This script imports tests the Streamlit-Authenticator package.
 
-with open('config.yaml') as file:
+Libraries imported:
+- yaml: Module implementing the data serialization used for human readable documents.
+- streamlit: Framework used to build pure Python web applications.
+"""
+
+import yaml
+import streamlit as st
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+
+# Loading config file
+with open('config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
-stauth.Hasher.hash_passwords(config['credentials'])
+# stauth.Hasher.hash_passwords(config['credentials'])
 
+# Creating the authenticator object
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    auto_hash=False
+    config['cookie']['expiry_days']
 )
 
-# login widget
+# authenticator = stauth.Authenticate(
+#     '../config.yaml'
+# )
+
+# Creating a login widget
 try:
-    authenticator.login(clear_on_submit=True, key="Login")
-except Exception as e:
+    authenticator.login()
+except LoginError as e:
     st.error(e)
 
-# authenticating users and logout button
+# Creating a guest login button
+try:
+    authenticator.experimental_guest_login('Login with Google', provider='google',
+                                            oauth2=config['oauth2'])
+    # authenticator.experimental_guest_login('Login with Microsoft', provider='microsoft',
+    #                                         oauth2=config['oauth2'])
+except LoginError as e:
+    st.error(e)
+
+# Authenticating user
 if st.session_state['authentication_status']:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
@@ -33,20 +62,57 @@ elif st.session_state['authentication_status'] is False:
 elif st.session_state['authentication_status'] is None:
     st.warning('Please enter your username and password')
 
-# reset password widget
+# Creating a password reset widget
 if st.session_state['authentication_status']:
     try:
         if authenticator.reset_password(st.session_state['username']):
             st.success('Password modified successfully')
-    except Exception as e:
+    except (CredentialsError, ResetError) as e:
         st.error(e)
 
-# new user registration widget
+# Creating a new user registration widget
 try:
-    email_of_registered_user, \
-    username_of_registered_user, \
-    name_of_registered_user = authenticator.register_user(pre_authorized=config['pre-authorized'])
+    (email_of_registered_user,
+        username_of_registered_user,
+        name_of_registered_user) = authenticator.register_user()
     if email_of_registered_user:
         st.success('User registered successfully')
-except Exception as e:
+except RegisterError as e:
     st.error(e)
+
+# Creating a forgot password widget
+try:
+    (username_of_forgotten_password,
+        email_of_forgotten_password,
+        new_random_password) = authenticator.forgot_password()
+    if username_of_forgotten_password:
+        st.success('New password sent securely')
+        # Random password to be transferred to the user securely
+    elif not username_of_forgotten_password:
+        st.error('Username not found')
+except ForgotError as e:
+    st.error(e)
+
+# Creating a forgot username widget
+try:
+    (username_of_forgotten_username,
+        email_of_forgotten_username) = authenticator.forgot_username()
+    if username_of_forgotten_username:
+        st.success('Username sent securely')
+        # Username to be transferred to the user securely
+    elif not username_of_forgotten_username:
+        st.error('Email not found')
+except ForgotError as e:
+    st.error(e)
+
+# Creating an update user details widget
+if st.session_state['authentication_status']:
+    try:
+        if authenticator.update_user_details(st.session_state['username']):
+            st.success('Entry updated successfully')
+    except UpdateError as e:
+        st.error(e)
+
+# Saving config file
+with open('config.yaml', 'w', encoding='utf-8') as file:
+    yaml.dump(config, file, default_flow_style=False)
