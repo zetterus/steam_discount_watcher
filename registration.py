c611c8b273c1336de60d381c45d7ad91b8097eef
zetterus <zetter.vitrolic@gmail.com>
2024-10-09 16:21:43 +0300

    order of messages in user settings, save settings button
    cookie settings save?
    autosaving?

--- registration.py
+++ registration.py
@@ -35,38 +35,38 @@ try:
 except RegisterError as e:
     st.error(e)
 
-# Creating a forgot password widget
-try:
-    (username_of_forgotten_password,
-     email_of_forgotten_password,
-     new_random_password) = st.session_state.authenticator.forgot_password(clear_on_submit=True)
-    if username_of_forgotten_password:
-        st.success('New password sent securely')
-        # Random password to be transferred to the user securely
-    elif not username_of_forgotten_password:
-        st.error('Username not found')
-except ForgotError as e:
-    st.error(e)
-
-# Creating a forgot username widget
-try:
-    (username_of_forgotten_username,
-     email_of_forgotten_username) = st.session_state.authenticator.forgot_username(clear_on_submit=True)
-    if username_of_forgotten_username:
-        st.success('Username sent securely')
-        # Username to be transferred to the user securely
-    elif not username_of_forgotten_username:
-        st.error('Email not found')
-except ForgotError as e:
-    st.error(e)
-
-# Creating an update user details widget
-if st.session_state['authentication_status']:
-    try:
-        if st.session_state.authenticator.update_user_details(st.session_state['username']):
-            st.success('Entry updated successfully')
-    except UpdateError as e:
-        st.error(e)
+# # Creating a forgot password widget
+# try:
+#     (username_of_forgotten_password,
+#      email_of_forgotten_password,
+#      new_random_password) = st.session_state.authenticator.forgot_password(clear_on_submit=True)
+#     if username_of_forgotten_password:
+#         st.success('New password sent securely')
+#         # Random password to be transferred to the user securely
+#     elif not username_of_forgotten_password:
+#         st.error('Username not found')
+# except ForgotError as e:
+#     st.error(e)
+#
+# # Creating a forgot username widget
+# try:
+#     (username_of_forgotten_username,
+#      email_of_forgotten_username) = st.session_state.authenticator.forgot_username(clear_on_submit=True)
+#     if username_of_forgotten_username:
+#         st.success('Username sent securely')
+#         # Username to be transferred to the user securely
+#     elif not username_of_forgotten_username:
+#         st.error('Email not found')
+# except ForgotError as e:
+#     st.error(e)
+#
+# # Creating an update user details widget
+# if st.session_state['authentication_status']:
+#     try:
+#         if st.session_state.authenticator.update_user_details(st.session_state['username']):
+#             st.success('Entry updated successfully')
+#     except UpdateError as e:
+#         st.error(e)
 
 # Saving config file
 config = st.session_state.config
--- user_settings.py
+++ user_settings.py
@@ -25,7 +25,8 @@ def session_state_to_dict(session_state):
     for key, value in session_state.items():
         # Handle different data types appropriately
         data[key] = value
-    return {st.session_state["str_var"]: data}
+    print(st.session_state)
+    return {st.session_state["name"]: data}
 
 
 # Custom deserialization function
@@ -35,6 +36,7 @@ def dict_to_session_state(data):
     for key, value in data.items():
         # Set values in SessionStateProxy
         session_state[key] = value
+    print(session_state)
     return session_state
 
 
@@ -48,9 +50,13 @@ def dict_to_session_state(data):
 # Creating a login widget
 try:
     st.session_state.authenticator.login()
-    with open('test_settings.py', 'r', encoding='utf-8') as file3:
+    with open('config.yaml', 'r', encoding='utf-8') as file3:
         data = yaml.load(file3, Loader=SafeLoader)
-        st.session_state = dict_to_session_state(data[st.session_state["name"]])
+    print(st.session_state.__str__())
+    try:
+        st.session_state = dict_to_session_state(data["credentials"][st.session_state["username"]])
+    except:
+        st.write("No user settings stored")
 except LoginError as e:
     st.error(e)
 
@@ -72,7 +78,7 @@ if st.session_state['authentication_status']:
     st.write(f'Welcome *{st.session_state["name"]}*')
     if st.button("Save watcher settings"):
         settings = session_state_to_dict(st.session_state)
-        with open('test_settings.py', 'w', encoding='utf-8') as file2:
+        with open('config.yaml', 'w', encoding='utf-8') as file2:
             yaml.dump(settings, file2, default_flow_style=False)
     if authenticator.logout():
         st.session_state.clear()
